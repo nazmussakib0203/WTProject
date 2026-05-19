290ae11aa7277016897725b33b97f3e39d7fc58b
shuvo2408 <shuvoban900@gmail.com>
2026-05-19 23:50:57 +0600

    add index file

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+?>
+
+<DOCTYPE html>
+<html>
+<head>
+    <title>Online Book Store</title>
+</head>
+<body>
+<h1>Online Book Store</h1>
+
+<h2>Task 4 - Checkout System</h2>
+
+<a href="checkout.php">Go To Checkout</a><br><br>
+
+<a href="purchase_history.php">Purchase History</a>
+
+</body>
+</html>
